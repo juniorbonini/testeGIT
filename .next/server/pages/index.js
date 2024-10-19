42b7b30dbe88d5328119c59b1d177a99da400aea
Luiz Augusto <juniorboninia7x@gmail.com>
2024-10-19 20:19:33 +0000

    Aciona o arquivo .gitignore

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"Pirmeira p\\xe1gina upada porra\"\n            }, void 0, false, {\n                fileName: \"/workspaces/testeGIT/pages/index.js\",\n                lineNumber: 4,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h3\", {\n                children: \"Voc\\xea me ajuda muito Kak\\xe1, espero que saiba que te considero a pessoa mais INCRIVEL da porra toda kkkk, te admiro PROFUNDAMENTE, pretendo focar legal e chegar onde eu sempre te prometi chegar VAMO PORRA, FOGUETE N\\xc3O TEM R\\xc9 KKKKK\"\n            }, void 0, false, {\n                fileName: \"/workspaces/testeGIT/pages/index.js\",\n                lineNumber: 5,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n                children: \"TE AMO PORRAAAAAAA SZSZ\"\n            }, void 0, false, {\n                fileName: \"/workspaces/testeGIT/pages/index.js\",\n                lineNumber: 8,\n                columnNumber: 5\n            }, this)\n        ]\n    }, void 0, true);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTztJQUNaLHFCQUNBOzswQkFDQSw4REFBQ0M7MEJBQUc7Ozs7OzswQkFDSiw4REFBQ0M7MEJBQUc7Ozs7OzswQkFHSiw4REFBQ0Q7MEJBQUc7Ozs7Ozs7O0FBR1I7QUFFQSxpRUFBZUQsSUFBSUEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3Rlc3RlLWRlcGxveS8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gSG9tZSgpIHtcbiAgICByZXR1cm4gKFxuICAgIDw+XG4gICAgPGgxPlBpcm1laXJhIHDDoWdpbmEgdXBhZGEgcG9ycmE8L2gxPlxuICAgIDxoMz5cbiAgICAgICAgVm9jw6ogbWUgYWp1ZGEgbXVpdG8gS2Frw6EsIGVzcGVybyBxdWUgc2FpYmEgcXVlIHRlIGNvbnNpZGVybyBhIHBlc3NvYSBtYWlzIElOQ1JJVkVMIGRhIHBvcnJhIHRvZGEga2traywgdGUgYWRtaXJvIFBST0ZVTkRBTUVOVEUsIHByZXRlbmRvIGZvY2FyIGxlZ2FsIGUgY2hlZ2FyIG9uZGUgZXUgc2VtcHJlIHRlIHByb21ldGkgY2hlZ2FyIFZBTU8gUE9SUkEsIEZPR1VFVEUgTsODTyBURU0gUsOJIEtLS0tLXG4gICAgPC9oMz5cbiAgICA8aDE+VEUgQU1PIFBPUlJBQUFBQUFBIFNaU1o8L2gxPlxuICAgIDwvPlxuICAgIClcbn1cblxuZXhwb3J0IGRlZmF1bHQgSG9tZTsiXSwibmFtZXMiOlsiSG9tZSIsImgxIiwiaDMiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+    <>
+    <h1>Pirmeira página upada porra</h1>
+    <h3>
+        Você me ajuda muito Kaká, espero que saiba que te considero a pessoa mais INCRIVEL da porra toda kkkk, te admiro PROFUNDAMENTE, pretendo focar legal e chegar onde eu sempre te prometi chegar VAMO PORRA, FOGUETE NÃO TEM RÉ KKKKK
+    </h3>
+    <h1>TE AMO PORRAAAAAAA SZSZ</h1>
+    </>
+    )
+}
+
+export default Home;
